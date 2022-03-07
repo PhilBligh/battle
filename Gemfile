a48d08f1adb0f72174c90bffcd9aed5ef73919c3
Joe Thornton <thornton.jp@gmail.com>
2022-03-07 15:19:30 +0000

    added sinatra reloader

--- Gemfile
+++ Gemfile
@@ -4,5 +4,6 @@ source "https://rubygems.org"
 
 gem 'sinatra'
 gem 'webrick'
+gem 'sinatra-contrib'
 
 # gem "rails"
--- app.rb
+++ app.rb
@@ -1,1 +1,18 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
+
+get '/' do
+  "Hello World"
+end
+
+get '/secret' do
+  "Secret greeting"
+end
+
+get '/news' do
+  "News site"
+end
+
+get '/sport' do
+  "Sport site"
+end
